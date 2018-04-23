83312274cdba47b81a2187816c1ab9836595fa8d
Exhex <c.kritik@gmail.com>
2018-04-23 12:21:15 +0200

    Added forced landscape mode

--- app/src/main/java/b135/rowbot_p2/MainActivity.java
+++ app/src/main/java/b135/rowbot_p2/MainActivity.java
@@ -1,7 +1,10 @@
 package b135.rowbot_p2;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set window fullscreen and remove title bar, and force landscape orientation
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
