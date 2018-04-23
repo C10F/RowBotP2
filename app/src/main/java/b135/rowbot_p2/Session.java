529c55c2892029ae1545bcc4fa55729d30b183b3
nicolai <nicolaidkz-wow@hotmail.com>
2018-04-24 00:19:55 +0200

    created the new layout and made a timer

--- app/src/main/java/b135/rowbot_p2/Session.java
+++ app/src/main/java/b135/rowbot_p2/Session.java
@@ -1,11 +1,18 @@
 package b135.rowbot_p2;
 
 import android.content.pm.ActivityInfo;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Chronometer;
 
 public class Session extends AppCompatActivity {
+
+    private Chronometer sessionTimer;
+    private long pauseOffset;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,26 @@ public class Session extends AppCompatActivity {
         // force landscape mode
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        sessionTimer = findViewById(R.id.timerCounter);
+        sessionTimer.setFormat("Time : %s");
+        sessionTimer.setBase(SystemClock.elapsedRealtime());
+
+    }
+
+    public void startTimerCounter(View v) {
+        if (!running) {
+            sessionTimer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            sessionTimer.start();
+            running = true;
+        }
+    }
+
+    public void pauseTimerCounter(View v) {
+        if (running) {
+            sessionTimer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - sessionTimer.getBase();
+            running = false;
+        }
     }
 }
