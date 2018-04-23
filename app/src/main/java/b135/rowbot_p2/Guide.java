b15b931d9ed39e8619fbdadeef57501ee0560044
Exhex <c.kritik@gmail.com>
2018-04-23 20:19:52 +0200

    added video button (visibility) /CF

--- app/src/main/java/b135/rowbot_p2/Guide.java
+++ app/src/main/java/b135/rowbot_p2/Guide.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 public class Guide extends AppCompatActivity {
 
+    boolean videoShowing;
+    boolean setupShowing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +22,26 @@ public class Guide extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
+
     public void showVideo(View view){
-        // set our video-view and title to be visible on button press
         ImageView videoPH = findViewById(R.id.embedVideoPH);
         TextView videoTxt = findViewById(R.id.videoText);
-
+        // set our video-view and title to be visible on button press
         videoPH.setVisibility(View.VISIBLE);
         videoTxt.setVisibility(View.VISIBLE);
+        videoShowing = true;
+    }
 
+
+    public void showSetup(View view){
+        ImageView videoPH = findViewById(R.id.embedVideoPH);
+        TextView videoTxt = findViewById(R.id.videoText);
+        // check for other views showing
+        if (videoShowing) {
+           videoPH.setVisibility(View.GONE);
+           videoTxt.setVisibility(View.GONE);
+           videoShowing = false;
+        }
+        // set our setup guide to be visible on button press
     }
 }
