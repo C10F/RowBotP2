70e67bbab1ddf9cc6ded95de37fd6bc0415133e6
nicolai <nicolaidkz-wow@hotmail.com>
2018-04-24 22:01:16 +0200

    made it so when a selection happens in the spinner different things happen part 2

--- app/src/main/java/b135/rowbot_p2/SessionInput.java
+++ app/src/main/java/b135/rowbot_p2/SessionInput.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ public class SessionInput extends AppCompatActivity implements AdapterView.OnIte
     private TextInputLayout sessionInputD;
     private TextInputLayout sessionInputT;
     private TextView selected;
+    private Button goTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +56,67 @@ public class SessionInput extends AppCompatActivity implements AdapterView.OnIte
     //this method is from the implemented interface and determines what happens once an item is selected
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //casts the variable selected to a TextView object by finding it's id
         selected = (TextView) findViewById(R.id.selectedPresetText);
-        if (position == 1) {
+        //same as above just for a button instead
+        goTo = (Button) findViewById(R.id.continueTo);
+        //sets a specific text in the textView and make the button disappear when position is 0
+        if (position == 0) {
+            selected.setText(R.string.SessionInputPresetNone);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.GONE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        //the 6 following if statements do the same sets a specific text in the textView and makes the button appear when position is 1-6
+        else if (position == 1) {
             selected.setText(R.string.SessionInputPresetSelection1);
             selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        else if (position == 2) {
+            selected.setText(R.string.SessionInputPresetSelection2);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        else if (position == 3) {
+            selected.setText(R.string.SessionInputPresetSelection3);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        else if (position == 4) {
+            selected.setText(R.string.SessionInputPresetSelection4);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        else if (position == 5) {
+            selected.setText(R.string.SessionInputPresetSelection5);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        else if (position == 6) {
+            selected.setText(R.string.SessionInputPresetSelection6);
+            selected.setVisibility(View.VISIBLE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.GONE);
+            sessionInputT.setVisibility(View.GONE);
+        }
+        //makes the textView disappear and makes the TextInputLayouts appear
+        else if (position == 7) {
+            selected.setVisibility(View.GONE);
+            goTo.setVisibility(View.VISIBLE);
+            sessionInputD.setVisibility(View.VISIBLE);
+            sessionInputT.setVisibility(View.VISIBLE);
         }
     }
 
