d49d6b76cd158e7c7a331840afa03dbf7e329533
nicolai <nicolaidkz-wow@hotmail.com>
2018-05-02 23:58:38 +0200

    changed feedback in the session view from mid fi test so now the buttons light up with the correct color and it has a stop button

--- app/src/main/java/b135/rowbot_p2/Session.java
+++ app/src/main/java/b135/rowbot_p2/Session.java
@@ -2,11 +2,13 @@ package b135.rowbot_p2;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
@@ -19,6 +21,8 @@ public class Session extends AppCompatActivity {
     private long pauseOffset;
     //boolean used to see whether or not the timer is running
     private boolean running;
+    //boolean used to see whether or not the stop button is available and if true then the session is able to stop
+    private boolean sessionStop = false;
     String targetDistance;
     String targetTime;
 
@@ -54,8 +58,17 @@ public class Session extends AppCompatActivity {
 
     //when you click the button start it's onClick listener starts this method to run the timer
     public void startTimerCounter(View v) {
+        //initializing buttons by id
+        Button sButton = findViewById(R.id.startSession);
+        Button pButton = findViewById(R.id.pauseSession);
         //if the boolean is false i.e. not running the timer will start
         if (!running) {
+            //sets various colors and texts
+            sButton.setBackgroundColor(getResources().getColor(R.color.green));
+            sButton.setTextColor(getResources().getColor(R.color.black));
+            pButton.setBackgroundColor(getResources().getColor(R.color.black));
+            pButton.setTextColor(getResources().getColor(R.color.white));
+            pButton.setText(R.string.PauseSession);
             //this makes the chronometer start counting when we click the button and not from when we start the activity which is the default
             //we subtract the offset to make sure that if we pause it, when we the run it again it will
             //continue from where we left and not keep counting in the background
@@ -63,6 +76,8 @@ public class Session extends AppCompatActivity {
             sessionTimer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             //starts the chronometer i.e. the timer and makes the timer text continue
             sessionTimer.start();
+            //sets sessionStop to false to make sure that when pause timer is called it won't stop but just pause
+            sessionStop = false;
             //sets running to true so that when you press pause the if statement is active
             running = true;
         }
@@ -70,8 +85,17 @@ public class Session extends AppCompatActivity {
 
     //when you click the button pause it's onClick listener starts this method to pause the timer
     public void pauseTimerCounter(View v) {
+        //initializing buttons by id
+        Button sButton = findViewById(R.id.startSession);
+        Button pButton = findViewById(R.id.pauseSession);
         //if the boolean is true i.e. running the timer will pause
         if (running) {
+            //sets various colors and texts
+            sButton.setBackgroundColor(getResources().getColor(R.color.black));
+            sButton.setTextColor(getResources().getColor(R.color.white));
+            pButton.setBackgroundColor(getResources().getColor(R.color.red));
+            pButton.setTextColor(getResources().getColor(R.color.black));
+            pButton.setText(R.string.StopSession);
             //pauses the chronometer i.e. the timer and stops the timer text without resetting it.
             sessionTimer.stop();
             //this resets the elapsedRealtime to zero since it take the time that has elapsed so far and subtract that by getting the base
@@ -79,8 +103,14 @@ public class Session extends AppCompatActivity {
             //this makes it so that once we start the timer it start from 0 again through the setBase
             //this is the counter that resets to zero NOT the text the timer text stays the same.
             pauseOffset = SystemClock.elapsedRealtime() - sessionTimer.getBase();
+            //sessionStop set to true to make sure that when stop is clicked it will be able to stop the session
+            sessionStop = true;
             //sets running to false so that when you press start the if statement is active
             running = false;
+        }
+        else if (sessionStop) {
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
         }
     }
 }
