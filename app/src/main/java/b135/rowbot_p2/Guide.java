2e2790ca964a3c573756f3c92742e35d60ad5289
Nicolai <nicolaidkz-wow@hotmail.com>
2018-05-09 14:43:19 +0200

    made guide look like stats

--- app/src/main/java/b135/rowbot_p2/Guide.java
+++ app/src/main/java/b135/rowbot_p2/Guide.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +30,10 @@ public class Guide extends AppCompatActivity {
         ImageView videoPH = findViewById(R.id.embedVideoPH);
         ImageView setupPH = findViewById(R.id.setupPH);
         TextView videoTxt = findViewById(R.id.videoText);
+        TextView startText = findViewById(R.id.guideStartText);
+        Button vButton = findViewById(R.id.videoButton);
+        Button sButton = findViewById(R.id.setupButton);
+        Button tButton = findViewById(R.id.TermButton);
         // check for other views showing
         if (setupShowing) {
             setupPH.setVisibility(View.GONE);
@@ -38,6 +43,10 @@ public class Guide extends AppCompatActivity {
             termPH.setVisibility(View.GONE);
             termShowing = false;
         }
+        vButton.setBackgroundResource(R.drawable.statistics_buttons_selected);
+        tButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        sButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        startText.setVisibility(View.GONE);
         // set our video-view and title to be visible on button press
         videoPH.setVisibility(View.VISIBLE);
         videoTxt.setVisibility(View.VISIBLE);
@@ -50,6 +59,10 @@ public class Guide extends AppCompatActivity {
         ImageView videoPH = findViewById(R.id.embedVideoPH);
         ImageView setupPH = findViewById(R.id.setupPH);
         TextView videoTxt = findViewById(R.id.videoText);
+        TextView startText = findViewById(R.id.guideStartText);
+        Button vButton = findViewById(R.id.videoButton);
+        Button sButton = findViewById(R.id.setupButton);
+        Button tButton = findViewById(R.id.TermButton);
 
         // check for other views showing
         if (videoShowing) {
@@ -61,6 +74,10 @@ public class Guide extends AppCompatActivity {
             termPH.setVisibility(View.GONE);
             termShowing = false;
         }
+        sButton.setBackgroundResource(R.drawable.statistics_buttons_selected);
+        tButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        vButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        startText.setVisibility(View.GONE);
         // set our setup guide to be visible on button press
         setupPH.setVisibility(View.VISIBLE);
         setupShowing = true;
@@ -71,6 +88,10 @@ public class Guide extends AppCompatActivity {
         ImageView videoPH = findViewById(R.id.embedVideoPH);
         ImageView setupPH = findViewById(R.id.setupPH);
         TextView videoTxt = findViewById(R.id.videoText);
+        TextView startText = findViewById(R.id.guideStartText);
+        Button vButton = findViewById(R.id.videoButton);
+        Button sButton = findViewById(R.id.setupButton);
+        Button tButton = findViewById(R.id.TermButton);
 
         // check for other views showing
         if(videoShowing){
@@ -82,6 +103,10 @@ public class Guide extends AppCompatActivity {
             setupPH.setVisibility(View.GONE);
             setupShowing = false;
         }
+        tButton.setBackgroundResource(R.drawable.statistics_buttons_selected);
+        vButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        sButton.setBackgroundResource(R.drawable.statistics_buttons_deselected);
+        startText.setVisibility(View.GONE);
         // set our terminology infographic to be visible on button press
         termPH.setVisibility(View.VISIBLE);
         termShowing = true;
